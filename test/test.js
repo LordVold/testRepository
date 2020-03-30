c0377fabde8eed465a3645b1227752aa1d218ccf
Lord Volodymyr <47691611+LordVold@users.noreply.github.com>
2020-03-30 17:23:29 +0300

    30.03-17:23

--- test/test.js
+++ test/test.js
@@ -15,3 +15,13 @@ let car1 = document.querySelector('.car1').innerText = audi.viewSpeed();
 audi = new Car(300);
 
 let car2 = document.querySelector('.car2').innerText = audi.viewSpeed();
+
+$(function() {
+  let x;
+  const myFunc = num => {
+    for (let i = 0; i < num; i++) console.log(i);
+  };
+
+  const result = (x = 3, x++, myFunc(x), x * 2);
+  console.log(`result: ${result}`);
+});
